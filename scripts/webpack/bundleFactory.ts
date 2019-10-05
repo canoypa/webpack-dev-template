77283173615bd225ab329818b57c4cb7c7d0128b
cano <mahorowa3088@gmail.com>
2019-10-05 11:34:21 +0900

    ⚡ 基本的なメソッドを作成

--- scripts/webpack/bundleFactory.ts
+++ scripts/webpack/bundleFactory.ts
@@ -1,3 +1,17 @@
-abstract class BundleFactory {}
+import * as webpack from 'webpack';
+
+abstract class BundleFactory {
+  createConfig(): webpack.Configuration {}
+
+  createCombined(): webpack.Configuration {
+    return this.createConfig();
+  }
+  createALaCarte(): webpack.Configuration {
+    return this.createConfig();
+  }
+
+  abstract getChunks(): WebpackChunks;
+  abstract getRules(): WebpackRules;
+}
 
 export default BundleFactory;
