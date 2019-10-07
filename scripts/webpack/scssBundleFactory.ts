2724caf08c43553466bbdb67837931f6e4c14050
cano <mahorowa3088@gmail.com>
2019-10-07 11:33:13 +0900

    ⚡ scss の出力

--- scripts/webpack/scssBundleFactory.ts
+++ scripts/webpack/scssBundleFactory.ts
@@ -1,5 +1,53 @@
-import BundleFactory from './bundleFactory';
+import * as webpack from 'webpack';
 
-class ScssBundleFactory extends BundleFactory {}
+import BundleFactory from './bundleFactory';
+import pathResolver from '../util/pathResolver';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import { ConfigOpt } from './type';
+
+class ScssBundleFactory implements BundleFactory {
+  createConfig(opt: ConfigOpt): webpack.Configuration {
+    return {
+      mode: 'production',
+
+      output: {
+        filename: 'none', // css の出力が js ファイルを上書きするのを防止する (css ファイルは MiniCssExtractPlugin が出力)
+        path: opt.output
+      },
+
+      entry: opt.chunks,
+
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+          }
+        ]
+      },
+
+      plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })],
+
+      resolve: { extensions: ['.scss'] }
+    };
+  }
+
+  createCombined(): webpack.Configuration {
+    const output = pathResolver.get('./build/Combined');
+    const chunks = pathResolver.getFromSrc('./index.scss');
+
+    return this.createConfig({ output, chunks });
+  }
+
+  createALaCarte(): webpack.Configuration {
+    const output = pathResolver.get('./build/Combined');
+    const chunks = {
+      chunk: pathResolver.getFromSrc('./chunk/index.scss')
+    };
+
+    return this.createConfig({ output, chunks });
+  }
+}
 
 export default ScssBundleFactory;
