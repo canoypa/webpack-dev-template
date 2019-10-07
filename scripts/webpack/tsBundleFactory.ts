4f316e3ef459fdfac3b70325443d24740805051c
cano <mahorowa3088@gmail.com>
2019-10-07 11:31:01 +0900

    ⚡ 出力

--- scripts/webpack/tsBundleFactory.ts
+++ scripts/webpack/tsBundleFactory.ts
@@ -1,5 +1,50 @@
-import BundleFactory from './bundleFactory';
+import * as webpack from 'webpack';
 
-class TsBundleFactory extends BundleFactory {}
+import BundleFactory from './bundleFactory';
+import pathResolver from '../util/pathResolver';
+
+import { ConfigOpt } from './type';
+
+class TsBundleFactory implements BundleFactory {
+  createConfig(opt: ConfigOpt): webpack.Configuration {
+    return {
+      mode: 'production',
+
+      output: { filename: '[name].js', path: opt.output, library: 'lib', libraryTarget: 'umd' },
+
+      entry: opt.chunks,
+
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: ['ts-loader']
+          }
+        ]
+      },
+
+      resolve: { extensions: ['.ts'] },
+
+      devtool: 'source-map'
+    };
+  }
+
+  createCombined(): webpack.Configuration {
+    const output = pathResolver.get('./build/Combined');
+    const chunks = pathResolver.getFromSrc('./index.ts');
+
+    return this.createConfig({ output, chunks });
+  }
+
+  createALaCarte(): webpack.Configuration {
+    const output = pathResolver.get('./build/Combined');
+    const chunks = {
+      chunk: pathResolver.getFromSrc('./chunk/index.ts')
+    };
+
+    return this.createConfig({ output, chunks });
+  }
+}
 
 export default TsBundleFactory;
